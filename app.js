abb402468e356ac47fdd9a7768618117cf19a554
Minxing Li <haydenlee6677@gmail.com>
2021-03-28 21:03:40 +1100

    complete

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const app = Vue.createApp({
         return {
           firstName: 'Hayden',
           lastName: 'Lee',
-          email: 'haydenlee6677@gmail.com',
+          email: 'haydenlee@example.com',
           gender: 'male',
-          picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+          picture: 'https://spectrumspeech.com.au/wp-content/uploads/2019/06/pikachu-character.jpg'
         }
-      }
+    },
+    methods: {
+        async getUser(){
+            const res =await fetch('https://randomuser.me/api');
+            const {results}=await res.json();
+            this.firstName = results[0].name.first;
+            this.lastName = results[0].name.last;
+            this.email = results[0].email;
+            this.gender = results[0].gender;
+            this.picture = results[0].picture.large;
+        }
+    },
 });
 
 app.mount('#app');
